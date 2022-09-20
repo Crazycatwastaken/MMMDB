062368e1a7846469e1a9c08d4b1c5462e5029c6a
Crazycatwastaken <maxrasmussen4@gmail.com>
2022-09-20 23:50:03 +1000

    Added Scrolling Left/Right

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+	import { element } from 'svelte/internal';
 	import { fetchDatabase } from '../stores/PopMoviesDB.js';
 
 	export let searchquery = '';
@@ -8,6 +9,30 @@
 		const data = await fetchDatabase();
 		return data.results;
 	}
+
+	let right = 5;
+	let left = 0;
+	// @ts-ignore
+	function scrollright({ target }) {
+		const el = document.querySelector(target.getAttribute('href'));
+		if (!el) return;
+		el.scrollIntoView({
+			behavior: 'smooth'
+		});
+		right++;
+		left = right - 5;
+	}
+	// @ts-ignore
+	function scrolleft({ target }) {
+		const el = document.querySelector(target.getAttribute('href'));
+		if (!el) return;
+		el.scrollIntoView({
+			behavior: 'smooth'
+		});
+		left--;
+		right = left + 5;
+	}
+
 	let promise = getdata();
 	let Poster = 'https://image.tmdb.org/t/p/original';
 </script>
@@ -37,17 +62,19 @@
 		{#await promise}
 			<p>loading...</p>
 		{:then data}
-			<div class="overflow-x-auto flex">
+			<div class="overflow-x-auto flex" style="overflow-x: hidden;">
 				{#each data as { poster_path, original_title }, i}
-				<div class="flex-none py-6 px-6 md:w-1/3 md:h-1/1   ">
-					<img
-						class="md:object-cover  rounded-xl gap-1"
-						src={Poster + poster_path}
-						alt="{original_title} poster"
-					/>
-				</div>
+					<div class="flex-none py-6 px-6 md:w-1/4 md:h-1/1" id="section-{i + 1}">
+						<img
+							class="md:object-cover rounded-xl"
+							src={Poster + poster_path}
+							alt="{original_title} poster"
+						/>
+					</div>
 				{/each}
 			</div>
 		{/await}
+		<a href="#section-{left}" on:click|preventDefault={scrolleft}>left</a>
+		<a href="#section-{right}" on:click|preventDefault={scrollright}>right</a>
 	</div>
 </main>
